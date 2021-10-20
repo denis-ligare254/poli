4a1fdbffe5c00edb2fb44ea0140852ccadbcc4f3
denis <mrisholigare@gmail.com>
2021-10-20 22:25:26 +0300

    all commits

---
+++ app.js
@@ -0,0 +1,162 @@
+
+// ********** set date ************
+// select span
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // linksContainer.classList.toggle("show-links");
+
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+  // console.log(linksContainer.getBoundingClientRect());
+});
+
+// ********** fixed navbar ************
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    linksContainer.style.height = 0;
+  });
+});
+// calculate heights
+// 
+// timer
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+const giveaway =document.querySelector('.giveaway');
+const deadline =document.querySelector('.deadline');
+const items = document.querySelector('.deadline-format h4');
+let futureDate =new Date(2022,07,09,08,30,00)
+let year= futureDate.getFullYear();
+let hours = futureDate.getHours();
+let minutes= futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month=months[month]
+const dateOne =futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent =`Election starts on ${weekday},${dateOne}${month} ${year} ${hours}:${minutes}am`;
+
+
+const futureTime = futureDate.getTime();
+function getRemaindingTime() {
+  const today = new Date().getTime();
+
+  const t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60m
+  // 1d = 24hr
+  // values in miliseconds
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  // calculate all values
+  let days = t / oneDay;
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+  // function format(item) {
+  //   if (item < 10) {
+  //     return (item = `0${item}`);
+  //   }
+  //   return item;
+  // }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = values[index];
+  });
+
+  // if (t < 0) {
+  //   clearInterval(countdown);
+  //   deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+  // }
+}
+// countdown;
+// let countdown = setInterval(getRemaindingTime, 1000);
+//set initial values
+getRemaindingTime();
